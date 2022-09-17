23a1dc5c3db2ffc4bc163469ca9c53dc05c9d290
krist7599555 <krist7599555@gmail.com>
2022-09-18 01:40:30 +0700

    docs: new page

---
+++ docs/_app/immutable/chunks/1-88bcdefd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-3878d246.js";export{t as component};
--- docs/_app/immutable/chunks/2-41fb2d7f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-66ead88e.js";export{t as component};
